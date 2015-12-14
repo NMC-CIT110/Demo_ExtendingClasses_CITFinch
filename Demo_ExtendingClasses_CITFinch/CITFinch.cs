d61abd23e9930acdb322fbcff99f2ae2088ea23f
John Velis <john@johnvelis.com>
2015-12-14 07:39:52 -0500

    commit739

---
+++ Demo_ExtendingClasses_CITFinch/CITFinch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinchAPI;
+
+namespace Demo_ExtendingClass_MyFinch
+{
+    public class CITFinch : Finch
+    {
+
+
+
+        public CITFinch()
+        {
+            base.connect();
+            base.setLED(255, 0, 0);
+        }
+    }
+}
---
+++ Demo_ExtendingClasses_CITFinch/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinchAPI;
+
+namespace Demo_ExtendingClass_MyFinch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CITFinch myFinch = new CITFinch();
+
+            Console.ReadKey();
+
+            myFinch.disConnect();
+        }
+    }
+}
